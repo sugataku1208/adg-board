2fab44f12a25ca64cadc9d09930b123fcace9d38
sugataku1208 <taku.taku701@gmail.com>
2019-03-22 18:50:56 +0900

    3/22

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <title>掲示板</title>
     <script language="javascript" type="text/javascript">
         function nullcheck() {
 
             var name=document.getElementById("name").value;
+
             var name_error=document.getElementById("name_error");
             var comment=document.getElementById("comment").value;
             var comment_error=document.getElementById("comment_error");
@@ -20,6 +21,7 @@
                 name_error.style.color="red";
                 name_error.innerHTML="名前を入力してください";
 
+
             }else{
                 name_error.style.display="none";
             }
@@ -28,17 +30,38 @@
                 comment_error.style.color="red";
                 comment_error.innerHTML="コメントを入力してください";
 
+
             }else{
                 comment_error.style.display="none";
             }
 
+            if($('#name,#comment').val().length == 0){
+                $('#bt').prop('disabled',true);
+            }
+
+            $('#name,#comment').on('keydown keyup keypress change',function(){
+                if($(this).val().length > 0){
+                    $('#name,#comment').prop('disabled',false);
+                }else{
+                    $('#bt').prop('disabled',false);
+                }
+            });
+
             if(name != "" && comment != ""){
                 complete.innerHTML="更新完了";
                 complete.style.display="block";
-                name.innerHTML="";
-                comment.innerHTML="";
-
+                $("#name,#comment").val("");
+            }else{
+                complete.style.display="none";
             }
+
+
+            $("ul").append("<li>"+name,comment+"</li>");
+
+
+
+
+
         }
     </script>
 
@@ -54,14 +77,19 @@
 
             コメント<textarea id="comment"name="text" id="" cols="30" rows="10" value=""></textarea><span id="comment_error"></span>
             <br>
-            <input id="bt" type="reset" name="reset"  type="button" value="更新" onclick="nullcheck();">
+            <input id="bt"   type="button" value="更新" onclick="nullcheck();">
 
         </form>
 
     </section>
     <section class="sub-area">
-        <h1>投稿一覧</h1>
-        <p>まだ投稿がありません</p>
+
+        <div>
+            <ul>
+
+            </ul>
+
+        </div>
     </section>
 </body>
 
