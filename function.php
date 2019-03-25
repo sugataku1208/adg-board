35a8b340fd4cba91d887b1ccd3a6313973c9377e
sugataku1208 <taku.taku701@gmail.com>
2019-03-25 17:47:45 +0900

    php commit

--- function.php
+++ function.php
@@ -5,44 +5,41 @@ $db_name = 'board_db';
 $db_user = 'board_user';
 $db_pass = 'board_pass';
 
-$link = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+$link = mysqli_connect( $db_host, $db_user, $db_pass, $db_name );
+if ( $link !== false ) {
+    $msg     = '';
+    $err_msg = '';
+    if ( isset( $_POST['send'] ) === true ) {
+        $name     = $_POST['name'];
+        $comment = $_POST['comment'];
+        if ( $name !== '' && $comment !== '' ) {
+            $query = " INSERT INTO board ( "
+                   . "    name , "
+                   . "    comment "
+                   . " ) VALUES ( "
+                   . "'" . mysqli_real_escape_string( $link, $name ) ."', "
+                   . "'" . mysqli_real_escape_string( $link, $comment ) . "'"
+                   ." ) ";
+            $res = mysqli_query( $link, $query );
+            if ( $res !== false ) {
+                $msg = '書き込み完了';
+            }else{
+                $err_msg = '書き込みに失敗しました';
+            }
+        }else{
+            $name_err_msg = '名前を記入してください';
+            $comment_err_msg = 'コメントを記入してください';
+        }
+    }
+    $query  = "SELECT id, name, comment FROM board";
+    $res    = mysqli_query( $link,$query );
+    $data = array();
+    while( $row = mysqli_fetch_assoc( $res ) ) {
+        array_push( $data, $row);
+    }
+    arsort( $data );
 
-$msg = '';
-$err_msg = '';
-
-if(isset( $_POST['send']) === true){
-    $name = $_POST['name'];
-    $comment = $_POST['comment'];
-
-    if( $name !== '' && $comment !== ''){
-
-        $query = "INSERT INTO board ("
-                . " name,"
-                ."comment"
-                .") VALUES ("
-                ."'". mysqli_real_escape_string( $link, $name) ."'"
-                ."'". mysql_real_escape_string($link,$comment)
-                ." ) ";
-
-                $res = mysqli_query($link, $qurey);
-
-                if( $res !== false){
-                    $msg ='書き込みに成功しました';
-                }else{
-                    $err_msg ="書き込みに失敗しました";
-                }
-             }else{
-                 $err_msg = '名前とコメントを記入してください';
-             }
-
-             $query = "SELECT id, name, comment, FROM board";
-             $res = mysqli_query( $link,$query);
-             $data = array();
-             while($row = mysqli_fetch_assoc( $res )){
-                 array_push($data, $row);
-             }
-             arsort( $data);
-}else{
+} else {
     echo "データベースの接続に失敗しました";
 }
 
--- index.php
+++ index.php
@@ -1,101 +1,33 @@
 <?php require("function.php");?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang = "ja">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <meta charset = "UTF-8">
+    <meta name = "viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv = "X-UA-Compatible" content = "ie=edge">
+    <link rel = "stylesheet" href = "style.css">
+    <script src = "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <title>掲示板</title>
-    <script language="javascript" type="text/javascript">
-        function emptycheck() {
-
-            var name=document.getElementById("name").value;
-            var name_error=document.getElementById("name_error");
-            var comment=document.getElementById("comment").value;
-            var comment_error=document.getElementById("comment_error");
-            var complete = document.getElementById("complete");
-
-            if(name == "" && comment == ""){
-
-                name_error.style.color="red";
-                name_error.innerHTML="名前を入力してください";
-                name_error.style.display="block";
-                comment_error.style.color="red";
-                comment_error.innerHTML="コメントを入力してください";
-                comment_error.style.display="block";
-                return false;
-            }
-            if(name == ""){
-
-                name_error.style.color="red";
-                name_error.innerHTML="名前を入力してください";
-                name_error.style.display="block";
-                return false;
-
-            }else{
-
-                name_error.style.display="none";
-
-            }
-
-            if(comment == ""){
-
-                comment_error.style.color="red";
-                comment_error.innerHTML="コメントを入力してください";
-                comment_error.style.display="block";
-
-                return false;
-
-            }else{
-
-                comment_error.style.display="none";
-
-            }
-
-            if(name != "" && comment != ""){
-
-                complete.innerHTML="更新完了";
-                $("#name,#comment").val("");
-
-            }else{
-
-                complete.style.display="none";
-
-            }
-
-            $("ul").append("<li>"+name,comment+"</li>");
-
-        }
-    </script>
-
 </head>
-<body id="main-form">
-    <section class="main-area">
-        <h2><span id = "complete"></span></h2>
+<body id = "main-form">
+    <section class = "main-area">
+       <?php if ( $msg !== '' ) echo '<p>' . $msg . '</p>'; ?>
         <h1>掲示板</h1>
-        <form action="" method="post">
-            <div class="name">
-                名前 <input id="name" type="text" name="name" autocomplete="off" value=""><span id="name_error"></span><br>
+        <form action = "" method = "post">
+            <div class = "name">
+                名前 <input id = "name" type = "text" name = "name" autocomplete = "off" value = ""><?php if ( $err_msg !== '' ) echo '<p style = "color:#FF0105;">' . $name_err_msg . '</p>'; ?><br>
             </div>
-
-            コメント<textarea id="comment"name="comment" id="" cols="30" rows="10" value=""></textarea><span id="comment_error"></span>
+            コメント<textarea id = "comment" name = "comment" cols = "30" rows = "10" value = ""></textarea><?php if ( $err_msg !== '' ) echo '<p style = "color:#FF0105;">' . $comment_err_msg . '</p>'; ?>
             <br>
-            <input id="bt"   type="button" value="更新" name="send" onclick="emptycheck();">
-
+            <input id = "bt" type="submit" value = "更新" name = "send" >
         </form>
-
     </section>
-    <section class="sub-area">
-
-        <div>
-            <ul>
-
-            </ul>
-
-        </div>
+    <section class = "sub-area">
+        <?php
+            foreach ($data as $key => $val) {
+                echo $val['name'] . '<br>' . $val['comment'] . '<br>';
+            }
+        ?>
     </section>
 </body>
-
 </html>
